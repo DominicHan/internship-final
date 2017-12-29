bf36e27d7414c31d79bbffc05ff3c71be87a1909
DominicHan <120493083@qq.com>
2017-12-29 10:10:26 +0800

    init project

---
+++ src/main/java/com/dominic/internshipfinal/InternshipFinalApplication.java
@@ -0,0 +1,12 @@
+package com.dominic.internshipfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipFinalApplication.class, args);
+	}
+}
